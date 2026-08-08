c335396556c6eb1172e9ff17ab78b60b7050b7ad
Don Martial <onebet.oneclick@gmail.com>
2026-08-08 11:39:26 +0100

    Update apiKeys.js

--- routes/apiKeys.js
+++ routes/apiKeys.js
@@ -15,6 +15,22 @@ const router = express.Router();
 |--------------------------------------------------------------------------
 */
 
-router.post("/generate", generateApiKey);
+import express from "express";
+
+import {
+    generateApiKey
+} from "../controllers/apiKeyController.js";
+
+import { requireAuth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.post(
+    "/generate",
+    requireAuth,
+    generateApiKey
+);
+
+export default router;
 
 export default router;
